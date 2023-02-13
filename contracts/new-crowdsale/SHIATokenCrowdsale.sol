e0d2b0e33430981d70fd45b42e5da4fcb095aee7
alabobriggs <alabobriggs@gmail.com>
2023-02-13 11:22:47 +0100

    rename to shia

--- contracts/new-crowdsale/SHIATokenCrowdsale.sol
+++ contracts/new-crowdsale/SHIATokenCrowdsale.sol
@@ -7,7 +7,7 @@ import "./Crowdsale.sol";
 import "./AllowanceCrowdsale.sol";
 import "./TimedCrowdsale.sol";
 
-contract ITManTokenCrowdsale is AllowanceCrowdsale, TimedCrowdsale {
+contract SHIATokenCrowdsale is AllowanceCrowdsale, TimedCrowdsale {
 	constructor(
 		uint256 _rate,
 		address payable _wallet,
