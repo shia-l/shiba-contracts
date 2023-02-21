b19bc4ab71a2fb140ec286ce1d0a7deb71acb342
alabobriggs <alabobriggs@gmail.com>
2023-02-21 18:27:03 +0100

    update shia contracts

--- contracts/SUSDT.sol
+++ contracts/SUSDT.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ITManToken is ERC20, Ownable {
-	constructor() ERC20("ITManToken", "ITM") {
+contract SUSDT is ERC20, Ownable {
+	constructor() ERC20("SUSDT", "SUSDT") {
 		_mint(msg.sender, 10000000000 * 10**decimals());
 	}
 
