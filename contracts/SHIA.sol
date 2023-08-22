be13e898c7a7dc69b21826686d0a6b491aee71f4
alabobriggs <alabobriggs@gmail.com>
2023-08-22 08:39:24 +0100

    rmove mint function

--- contracts/SHIA.sol
+++ contracts/SHIA.sol
@@ -8,8 +8,4 @@ contract SHIA is ERC20, Ownable {
 	constructor() ERC20("SHIA", "SHIA") {
 		_mint(msg.sender, 10000000000 * 10**decimals());
 	}
-
-	function mint(address to, uint256 amount) public onlyOwner {
-		_mint(to, amount);
-	}
 }
